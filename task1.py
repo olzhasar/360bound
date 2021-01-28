b12844bf54246a2daaea00f0e0ae531ef458cd7d
olzhasar <o.arystanov@gmail.com>
2021-01-29 05:01:03 +0600

    Updated task1

--- task1.py
+++ task1.py
@@ -6,29 +6,32 @@ def get_weights(arr: List[Dict[str, Union[int, List[int]]]]):
     if not arr:
         return []
 
-    weights: Dict[int, int] = {}
+    l = len(arr)
 
-    without_parent = list(range(1, len(arr) + 1))
+    weights: List[int] = [0] * l
 
-    children: Dict[int, List[int]] = {}
+    without_parent: List[int] = list(range(1, l + 1))
+
+    children_mapping: Dict[int, List[int]] = {}
 
     for row in arr:
-        weights[row["id"]] = row["weight"]
+        i = row["id"]
 
-        children[row["id"]] = []
+        weights[i - 1] = row["weight"]
+        children_mapping[i] = []
 
         for el in row["elems"]:
-            children[row["id"]].append(el)
+            children_mapping[i].append(el)
             without_parent.remove(el)
 
-    root = without_parent[0]
+    root_id = without_parent[0]
 
     def calc_weight(node_id: int):
-        for child in children[node_id]:
-            weights[node_id] += calc_weight(child)
+        for child in children_mapping[node_id]:
+            weights[node_id - 1] += calc_weight(child)
 
-        return weights[node_id]
+        return weights[node_id - 1]
 
-    calc_weight(root)
+    calc_weight(root_id)
 
-    return [weights[i] for i in sorted(weights.keys())]
+    return weights
