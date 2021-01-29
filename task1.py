4925487360ef81a1069f6ae44610eb2e89a16797
olzhasar <o.arystanov@gmail.com>
2021-01-29 15:59:03 +0600

    Updated task1

--- task1.py
+++ task1.py
@@ -7,20 +7,24 @@ def get_weights(arr: List[Row]) -> Dict[str, int]:
     if not arr:
         return []
 
-    weights: Dict[str, int] = {}
+    mapping: Dict[int, row] = {}
+
+    for row in arr:
+        i = row["id"]
+        row.pop("id")
+
+        mapping[i] = row
 
     def calc_weight(row: Row):
-        key = str(row["id"])
+        if not "full_weight" in row:
+            row["full_weight"] = row["weight"]
 
-        if key not in weights:
-            weights[key] = row["weight"]
+            for child in row["elems"]:
+                row["full_weight"] += calc_weight(mapping[child])
 
-            for child in filter(lambda x: x["id"] in row["elems"], arr):
-                weights[key] += calc_weight(child)
-
-        return weights[key]
+        return row["full_weight"]
 
     for row in arr:
         calc_weight(row)
 
-    return weights
+    return {str(key): value["full_weight"] for key, value in mapping.items()}
