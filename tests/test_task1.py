1fd77238e037e4bcd4923ccb57cf8c36b171e799
olzhasar <o.arystanov@gmail.com>
2021-01-29 13:17:26 +0600

    More tests

--- tests/test_task1.py
+++ tests/test_task1.py
@@ -18,6 +18,18 @@ import pytest
         ],
         [
             [
+                {"id": 1, "weight": 3, "elems": [5]},
+                {"id": 2, "weight": 1, "elems": []},
+                {"id": 3, "weight": 5, "elems": [4]},
+                {"id": 4, "weight": 1, "elems": [7]},
+                {"id": 5, "weight": 4, "elems": [2, 6]},
+                {"id": 6, "weight": 2, "elems": []},
+                {"id": 7, "weight": 2, "elems": []},
+            ],
+            [10, 1, 8, 3, 7, 2, 2],
+        ],
+        [
+            [
                 {"id": 1, "weight": 1, "elems": []},
             ],
             [1],
